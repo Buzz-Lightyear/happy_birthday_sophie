3eaa3dba049e7736fca11dd1f6ca29da64cef6ca
Srini Muthu <smuthu@apple.com>
2019-04-23 12:08:02 -0400

    Made updates to iFrame

---
+++ js/present.js
@@ -0,0 +1,5 @@
+document.getElementById('submit').onclick = function() {
+    var answer = document.getElementById('answer').value;
+    document.getElementById("gifts").style.display = "block";
+    return false;
+}
